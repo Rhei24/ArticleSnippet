a07379cfebec248ff74a7f6687c2c6d6fa8b0d71
Rhei24 <acostareyvincent@gmail.com>
2023-06-29 14:12:17 +0800

    removed Header and About

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import "./App.css";
-import About from "./components/about/About";
-import Header from "./components/header/Header";
+// import About from "./components/about/About";
+// import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
-      <Header />
       <Home />
-      <About />
     </div>
   );
 }
